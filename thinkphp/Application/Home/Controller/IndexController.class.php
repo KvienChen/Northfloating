c341ec47df6335dfc2a7ac36a007e03f909d8438
葛亚茹 <534@qq.com>
2016-03-24 13:54:11 +0800

    登录方法  和 验证码

--- thinkphp/Application/Home/Controller/IndexController.class.php
+++ thinkphp/Application/Home/Controller/IndexController.class.php
@@ -8,7 +8,7 @@ use Think\Controller;
     该控制器的模块方法已列出   显示页面为方法名加 .html
 */
 
-class IndexController extends Controller {
+class IndexController extends  Controller {
     /**
       *   显示主界面
       **/
@@ -30,6 +30,42 @@ class IndexController extends Controller {
         echo '我审核的房源信息';
     }
 
+    
+    /**
+      *   显示验证码 
+      **/
+    public function verify(){
+     $Verify = new \Think\Verify();
+     $Verify->length=2;
+     $Verify->entry();
+    }
 
+     /**
+      *   核实验证码   核实登录信息
+      **/
+    public function add_pro(){
+        $verify = new \Think\Verify();  
+        $re = $verify->check($_POST['verifycode'], $id);
+        if(!$re){
+          $this->error('验证码错误');
+        }else{
+          $AdminUser=D('AdminUser');
+          $re = $AdminUser->testpwd();
+          if($re){
+           $username = I('post.u_name');
+           session(user,$re); 
+           $this->success('登陆成功',__APP__.'/Home/index/index');
+          }else{
+            $this->error('登陆失败');
+          }
+        }
+     }
+   
+     /**
+      *   显示登录页面  
+      **/
+    public function log(){
+      $this->display('login');
+    }
 
 }
